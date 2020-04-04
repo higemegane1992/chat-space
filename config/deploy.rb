720814b7186ed55dba3f637b31fefac4364cedaf
higemegane1992 <kazuaki.hirata.0905@gmail.com>
2020-04-04 20:31:51 +0900

    install capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.12.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-app'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:higemegane1992/chat-space.git'
